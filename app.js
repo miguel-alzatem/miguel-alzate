a4820409147513f670d40bee15ffb3219e0242bf
miguel-alzatem <123768437+miguel-alzatem@users.noreply.github.com>
2023-03-13 12:40:53 -0500

    288254

--- app.js
+++ app.js
@@ -1,50 +1,45 @@
-//----------------- lecture_01 ----------------------------------// 
+//----------------- lecture_01 ----------------------------------//
 document.addEventListener("DOMContentLoaded", () => {
-//----------------- lecture_02 ----------------------------------//
-const cardAdj = [
-{name: "1.png", img: "images/1.png"},  
-{name: "2.png", img: "images/2.png"}, 
-{name: "3.png", img: "images/3.png"}, 
-{name: "4.png", img: "images/4.png"}, 
-{name: "5.png", img: "images/5.png"},
-{name: "6.png", img: "images/6.png"},
-{name: "1.png", img: "images/1.png"},  
-{name: "2.png", img: "images/2.png"}, 
-{name: "3.png", img: "images/3.png"}, 
-{name: "4.png", img: "images/4.png"}, 
-{name: "5.png", img: "images/5.png"},
-{name: "6.png", img: "images/6.png"},
- ];
-const cuadricula = document.querySelector(".cuadricula");
+  //----------------- lecture_02 ----------------------------------//
+  const cardAdj = [
+    { name: "1.png", img: "images/1.png" },
+    { name: "2.png", img: "images/2.png" },
+    { name: "3.png", img: "images/3.png" },
+    { name: "4.png", img: "images/4.png" },
+    { name: "5.png", img: "images/5.png" },
+    { name: "6.png", img: "images/6.png" },
+    { name: "1.png", img: "images/1.png" },
+    { name: "2.png", img: "images/2.png" },
+    { name: "3.png", img: "images/3.png" },
+    { name: "4.png", img: "images/4.png" },
+    { name: "5.png", img: "images/5.png" },
+    { name: "6.png", img: "images/6.png" }
+  ];
+
+  const cuadricula = document.querySelector(".cuadricula");
   const resultado = document.querySelector("#resultado");
   var cartasEscogidas = [];
   var cartasEscogidasId = [];
   var cartasGanadas = [];
 
-//----------------- lecture_03 ----------------------------------//  
-  function crearTablero (){
-    for (let i = 0; i < cardAdj.length; i++) { 
-      var carta = document.createElement('img'); 
-
-      carta.setAttribute('src', 'images/reverso.png'); 
-                                                       
-      carta.setAttribute('data-id', i); 
-
-      carta.addEventListener('click', voltearCarta); 
-                                                     
-      cuadricula.appendChild(carta); 
-                                     
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "images/reverso.png");
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
+      cuadricula.appendChild(carta);
     }
   }
-//----------------- lecture_04 ----------------------------------//
-   
-function voltearCarta() { 
-  var cardId = this.getAttribute("data-id");
-  cartasEscogidas.push(cardAdj[cardId].name);
-  cartasEscogidasId.push(carId);
-  this.setAttribute("src", cardAdj[cardId].img);
-  if (cartasEscogidas.length ===2) { 
-    setTimeout(verificarPareja, 1000);
+
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1500);
+    }
   }
   crearTablero();
 });
